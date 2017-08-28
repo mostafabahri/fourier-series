eba24793c084b0706e3c7b1e9c55fc9980ff0ebc
Mostafa Bahri <mostafabahri.dev@gmail.com>
2017-08-28 23:43:25 +0430

    add hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  section.hero.is-dark.is-bold
+    .hero-body
+      .container
+        h1.title {{title}}
+        h2.subtitle {{subtitle}}
+
+</template>
+
+<script>
+export default {
+  name: 'hero',
+  data() {
+    return {
+      title: 'Fourier Series',
+      subtitle: 'By Mostafa',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
