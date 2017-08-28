3330d0d70114396d3ad9069d24a2a5d8dd232fb6
Mostafa Bahri <mostafabahri.dev@gmail.com>
2017-08-28 23:43:53 +0430

    add bulma to main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import '../node_modules/bulma/css/bulma.css'
 
 Vue.config.productionTip = false;
 
