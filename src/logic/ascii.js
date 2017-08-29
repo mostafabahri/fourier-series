a60ab0878cacb8906c0a8cfbfce32fcbaae5d8c3
Mostafa Bahri <mostafabahri.dev@gmail.com>
2017-08-29 06:17:03 +0430

    add cosine series and some render touchup

--- src/logic/ascii.js
+++ src/logic/ascii.js
@@ -39,6 +39,7 @@ function run_me (f, x) {
 }
 
 function only_if_not_1 (x) {
+  // skip when number is 1
   if (x === 1) {
     return ''
   }
@@ -52,22 +53,37 @@ function coeffs_to_asciimath (coeffs, l, mode) {
   pi_l = Fourier.math_round(pi_l)
   pi_l = only_if_not_1(pi_l)
 
+  // using asciimath and string interpolation
+  // http://asciimath.org/ for syntax info
+
+  let a_0 = (a0) => `${a0}/2`
+  let a_n = (an, n) => `${an} cos(${n}*${pi_l}x)`
+  let b_n = (bn, n) => `${bn} sin(${n}*${pi_l}x)`
+
   switch (mode) {
     case 'normal' :
-      res.push([`${coeffs[0].a0}/2`, 0])
+      res.push([a_0(coeffs[0].a0), 0])
       for (let n = 1; n < coeffs.length; n++) {
         let {an, bn} = {an: coeffs[n].an, bn: coeffs[n].bn}
 
-        res.push([`${an} * cos(${n}*${pi_l}x)`,
-          `${bn} * sin(${n}*${pi_l}x)`])
+        res.push([a_n(an, n),
+                b_n(bn, n)])
       }
       break
     case 'sine' :
       res.push([0, 0])
       for (let n = 1; n < coeffs.length; n++) {
-        let {bn} = {bn: coeffs[n].bn}
+        let bn = coeffs[n].bn
         res.push([0,
-          `${bn} * sin(${n}*${pi_l}x)`])
+          b_n(bn, n)])
+      }
+      break
+    case 'cosine' :
+      res.push([a_0(coeffs[0].a0), 0])
+      for (let n = 1; n < coeffs.length; n++) {
+        let an = coeffs[n].an
+        res.push([a_n(an, n),
+                  0])
       }
   }
   // math simplify can help
@@ -75,7 +91,7 @@ function coeffs_to_asciimath (coeffs, l, mode) {
   return res
 }
 
-function view_wrapper (expr, low, high, count, mode = 'normal') {
+function FourierSeriesGeneralX (expr, low, high, count, mode = 'normal') {
 
   const mathexp = new MathExpression(expr)
   const low_eval = new MathExpression(low).evaluate(0)
@@ -90,7 +106,8 @@ function view_wrapper (expr, low, high, count, mode = 'normal') {
       coeffs = new Fourier.FourierSeriesSineCoeffs(mathexp.evaluate, low_eval, high_eval, count)
       break
     case 'cosine' :
-      throw Error('not implemented')
+      coeffs = new Fourier.FourierSeriesCosineCoeffs(mathexp.evaluate, low_eval, high_eval, count)
+      break
   }
   return coeffs_to_asciimath(coeffs, high, mode)
 }
@@ -99,9 +116,31 @@ function is_range_valid (low, high) {
   const low_eval = new MathExpression(low).evaluate(0)
   const high_eval = new MathExpression(high).evaluate(0)
 
-  return low_eval < high_eval;
+  return low_eval < high_eval
+
+}
+
+function FourierSeriesFX (expr, x, low, high, count , mode) {
+  const mathexp = new MathExpression(expr)
+  const low_eval = new MathExpression(low).evaluate(0)
+  const high_eval = new MathExpression(high).evaluate(0)
+  const x_eval = parseFloat(x)
+  let coeffs = 0
+  switch (mode) {
+    case 'normal' :
+      coeffs = new Fourier.FourierSeriesCoeffs(mathexp.evaluate, low_eval, high_eval, count)
+      break
+    case 'sine' :
+      coeffs = new Fourier.FourierSeriesSineCoeffs(mathexp.evaluate, low_eval, high_eval, count)
+      break
+    case 'cosine' :
+      coeffs = new Fourier.FourierSeriesCosineCoeffs(mathexp.evaluate, low_eval, high_eval, count)
+      break
+  }
+  return FourierSeriesValue(x_eval, coeffs, low, high)
 
 }
 export default {
-  view_wrapper, is_range_valid
+  is_range_valid,
+  FourierSeriesGeneralX,
 }
--- src/logic/fourier.js
+++ src/logic/fourier.js
@@ -1,91 +1,106 @@
-"use strict"
+'use strict'
 import math from 'mathjs'
 const pi = math.pi
 
-
-function math_round(x) {
-    return Math.round(x * 1000) / 1000
-    // return x
+function math_round (x) {
+  // round number up to 3 floating points
+  return Math.round(x * 1000) / 1000
+  // return x
 }
 
+function FourierSeriesCoeffs (f, low = -pi, high = pi, count = 3) {
 
-function FourierSeriesCoeffs(f, lower_bound = -pi, upper_bound = pi , count = 3){
+  let coeffs = []
 
-    let coeffs = []
-
-    let a_0 = constant_a0(f, lower_bound, upper_bound)
-    coeffs.push({a0 : math_round(a_0)})
-
-    for (let n = 1; n < count; n++) {
-        //series coeffs
-        coeffs.push({ an : math_round(a_n_clause(f, n, lower_bound, upper_bound))
-         	   	, bn : math_round(b_n_clause(f, n, lower_bound, upper_bound))})
-    }
-    return coeffs
+  let a_0 = constant_a0(f, low, high)
+  coeffs.push({a0: math_round(a_0)})
+  // calculating the sum
+  for (let n = 1; n < count; n++) {
+    //series coeffs
+    coeffs.push({
+      an: math_round(a_n_clause(f, n, low, high))
+      , bn: math_round(b_n_clause(f, n, low, high))
+    })
+  }
+  return coeffs
 }
 
-function FourierSeriesSineCoeffs(f, lower_bound = -pi, upper_bound = pi , count = 3){
+function FourierSeriesSineCoeffs (f, low = -pi, high = pi, count = 3) {
 
-    let coeffs = []
+  let coeffs = []
 
-    coeffs.push({a0 : 0})
+  coeffs.push({a0: 0})
 
-    for (let n = 1; n < count; n++) {
-        //series coeffs
-        coeffs.push({ an : 0
-                , bn : 2 * math_round(b_n_clause(f, n, 0, upper_bound))})
-    }
-    return coeffs
+  for (let n = 1; n < count; n++) {
+
+    coeffs.push({
+      an: 0
+      , bn: 2 * math_round(b_n_clause(f, n, 0, high))
+    })
+  }
+  return coeffs
 }
 
+function FourierSeriesCosineCoeffs (f, low = -pi, high = pi, count = 3) {
 
+  let coeffs = []
 
-// for f(x) where x = x0
-function FourierSeriesValue(x, coeffs, lower_bound, upper_bound){
+  let a_0 = 2 * constant_a0(f, low, high)
+  coeffs.push({a0: math_round(a_0)})
 
-    let acc  = 0
-    acc += coeffs[0].a0/2
-
-    for (let n = 1; n < coeffs.length; n++) {
-        acc += coeffs[n].an * Math.cos(n*pi/upper_bound*x) + coeffs[n].bn * Math.sin(n*pi/upper_bound*x)
-    }
-    return acc
-}
-function FourierSeriesFX(f, x, lower_bound, upper_bound, count = 3){
-    const coeffs = FourierSeriesCoeffs(f, lower_bound, upper_bound, count)
-    console.log(coeffs)
-    return FourierSeriesValue(x, coeffs , lower_bound, upper_bound)
+  for (let n = 1; n < count; n++) {
+    //series coeffs
+    coeffs.push({
+      an: 2 * math_round(a_n_clause(f, n, 0, high))
+      , bn: 0
+    })
+  }
+  return coeffs
 }
 
+// for f(x) where x = x0, given series coefficients
+function FourierSeriesValue (x, coeffs, low, high) {
 
-function definite_integrate(f, a, b, step = 0.0001) {
-    let acc = 0
+  let acc = 0
+  acc += coeffs[0].a0 / 2
+  // the pi/l in cos(n*pi/lx) and sin(n*pi/lx)
+  let pi_l = pi / high
 
-    for (let x = a; x < b; x+= step) {
-        acc +=  f(x + step / 2) * step;
-    }
-    return acc
+  for (let n = 1; n < coeffs.length; n++) {
+    acc += coeffs[n].an * Math.cos(n * pi_l * x) + coeffs[n].bn * Math.sin(n * pi_l * x)
+  }
+  return acc
 }
 
-function constant_a0(f, lower_bound, upper_bound) {
+function definite_integrate (f, a, b, step = 0.0001) {
+  let acc = 0
 
-    return a_n_clause(f, 0, lower_bound, upper_bound)
+  for (let x = a; x < b; x += step) {
+    acc += f(x + step / 2) * step
+  }
+  return acc
 }
 
-function a_n_clause(f, n, lower_bound, upper_bound) {
+function constant_a0 (f, low, high) {
 
-     return 1/ upper_bound * definite_integrate( function(x) {
-        return  f(x) * math.cos(n * pi * x/upper_bound) }
-        , lower_bound, upper_bound)
+  return a_n_clause(f, 0, low, high)
 }
 
-function b_n_clause(f, n, lower_bound, upper_bound){
+function a_n_clause (f, n, low, high) {
 
-     return 1/ upper_bound * definite_integrate( function(x) {
-        return  f(x) * math.sin(n * pi * x/upper_bound) }
-        , lower_bound, upper_bound)
+  return 1 / high * definite_integrate(function (x) {
+        return f(x) * math.cos(n * pi * x / high)
+      }
+      , low, high)
 }
 
+function b_n_clause (f, n, low, high) {
+
+  return 1 / high * definite_integrate(function (x) {
+        return f(x) * math.sin(n * pi * x / high)
+      }
+      , low, high)
+}
 
 // var h = 'x*(pi-x)'
 // var n = 6
@@ -99,13 +114,13 @@ function b_n_clause(f, n, lower_bound, upper_bound){
 
 //console.log(math.simplify(f).toString())
 
-
-function func(x){
-    return x*x + x;
+function func (x) {
+  return x * x + x
 }
 
-
 export default {
-  FourierSeriesCoeffs, math_round,
-  FourierSeriesSineCoeffs
+  math_round,
+  FourierSeriesCoeffs,
+  FourierSeriesSineCoeffs,
+  FourierSeriesCosineCoeffs
 }
