595fc3d797c1a89d3dca633b68d6c558baab6e33
Mostafa Bahri <mostafabahri.dev@gmail.com>
2017-08-29 04:32:53 +0430

    remove redundant stuff from fourier.js

--- src/logic/fourier.js
+++ src/logic/fourier.js
@@ -67,17 +67,6 @@ function definite_integrate(f, a, b, step = 0.0001) {
     return acc
 }
 
-// function trap_definite_integrate(f, a, b, step = 0.0001) {
-//     n = 10000
-//     let diff = (b - a) / n
-//     let series_acc  = 0
-//     for(let k = 0; k < n; k++){
-//         series_acc  += f(a + k*(diff))
-//     }
-//     return diff*(f(a)/2 + series_acc + f(b)/2)
-// }
-//    total += [a_n_clause(f, n, lower_bound, upper_bound) * math.cos(n*pi/upper_bound*x)
-//   	   + b_n_clause(f, n, lower_bound, upper_bound) * math.sin(n*pi/upper_bound*x)]
 function constant_a0(f, lower_bound, upper_bound) {
 
     return a_n_clause(f, 0, lower_bound, upper_bound)
@@ -98,21 +87,6 @@ function b_n_clause(f, n, lower_bound, upper_bound){
 }
 
 
- // example function f(x) = 3*x^2
-
-function coeffs_to_asciimath(coeffs, l = pi){
-    let res = []
-    l = math_round(l)
-    res.push([`${coeffs[0].a0}/2`, 0])
-    for (let i = 1; i < coeffs.length; i++) {
-        res.push([`${coeffs[i].an} * cos (${i}*pi*x/${l})`,
-        `${coeffs[i].bn} * sin (${i}*pi*x/${l})`])
-    }
-    return res
-}
-
-//console.log(b_n_clause(f,-pi, pi,1))
-
 // var h = 'x*(pi-x)'
 // var n = 6
 // var l = '2pi'
@@ -130,16 +104,6 @@ function func(x){
     return x*x + x;
 }
 
-//  console.log(definite_integrate(func, -pi , +pi)) // ~ 4
-//
-// let all_coeffs = FourierSeriesCoeffs(func, -pi, pi, 5)
-// let mapped_to_ml = coeffs_to_asciimath(all_coeffs, pi)
-// console.log(mapped_to_ml.map( x => `(${x[0]} + ${x[1]})`)
-//                         .reduce( (x, y) =>  `${x} + ${y}`))
-// console.log(FourierSeriesFX(func, 3, -pi, pi, 100))
-//
-//  const sineCoeffs = FourierSeriesSineCoeffs(func, -pi, pi , 10)
-//  console.log(sineCoeffs)
 
 export default {
   FourierSeriesCoeffs, math_round,
